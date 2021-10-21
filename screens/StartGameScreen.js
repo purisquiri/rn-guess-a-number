5bca9b3f328ffbc2953754d50315069837c768f4
purisquiri <mau.dorigo@outlook.es>
2021-10-21 15:51:04 -0400

    game screen showing when starting the game

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -51,7 +51,10 @@ const StartGameScreen = (props) => {
       <Card style={styles.chosenContainer}>
         <Text>You Selected</Text>
         <NumberContainer>{selectedNumber}</NumberContainer>
-        <Button title="START GAME" />
+        <Button
+          title="START GAME"
+          onPress={() => props.onStartGame(selectedNumber)}
+        />
       </Card>
     );
   }
